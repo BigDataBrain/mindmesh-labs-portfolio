2d670de0c81ba9799801e5cc564327a97a2ea5d0
Maclaine Fernandes <pybot2021@gmail.com>
2025-07-16 23:16:48 +0400

    Update_V6: revised components and styling tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mindmesh-labs-portfolio",
+  // base: "/mindmesh-labs-portfolio",
+  base: "/",
   build: {cssMinify: false,
     outDir: "dist",
     sourcemap: false,
