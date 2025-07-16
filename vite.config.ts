cedfedc0265fcbe6712f97377102e2e6e4a2c10f
Maclaine Fernandes <pybot2021@gmail.com>
2025-07-16 21:06:24 +0400

    Update3: revised components and fixed styling tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/mindmesh-labs-portfolio",
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  }
-})
+  // define: {
+    // 'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  // }
+});
